83859925f87abed355233a47e66602a35b9daf13
BogdanBoboaca <bogdan.m.boboaca@gmail.com>
2020-03-25 14:50:03 +0200

    Initial commit for the final project(Project Structure)

---
+++ src/main/java/org/fasttrackit/cjobs/CjobsApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.cjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CjobsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fasttrackit/cjobs/CjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.cjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CjobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
