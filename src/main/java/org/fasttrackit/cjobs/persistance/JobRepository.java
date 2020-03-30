cd1c98ef239212ae45e1ba84dda019b1083d878e
BogdanBoboaca <bogdan.m.boboaca@gmail.com>
2020-03-30 11:27:07 +0300

    added queries derived from method names

--- src/main/java/org/fasttrackit/cjobs/persistance/JobRepository.java
+++ src/main/java/org/fasttrackit/cjobs/persistance/JobRepository.java
@@ -4,13 +4,9 @@ import org.fasttrackit.cjobs.domain.Job;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
 
     Page<Job> findByNameContaining(String partialName, Pageable pageable);
 
-    @Query(value = "SELECT * FROM job WHERE `name` LIKE '%?0%'", nativeQuery = true)
-   //@Query("SELECT job FROM Job job WHERE `name` LIKE '%:partialName%'")
-    Page<Job> findByPartialName(String partialName, Pageable pageable);
 }
