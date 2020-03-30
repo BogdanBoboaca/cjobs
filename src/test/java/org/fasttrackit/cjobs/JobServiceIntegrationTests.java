00c37b71683614d4cc67669813d200cd6bb7b17a
BogdanBoboaca <bogdan.m.boboaca@gmail.com>
2020-03-30 10:44:23 +0300

    added tests for getJob method

--- src/test/java/org/fasttrackit/cjobs/JobServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/cjobs/JobServiceIntegrationTests.java
@@ -1,8 +1,10 @@
 package org.fasttrackit.cjobs;
 
 import org.fasttrackit.cjobs.domain.Job;
+import org.fasttrackit.cjobs.exception.ResourceNotFoundException;
 import org.fasttrackit.cjobs.service.JobService;
 import org.fasttrackit.cjobs.transfer.job.SaveJobRequest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,17 +23,10 @@ public class JobServiceIntegrationTests {
 
     @Test
     void createJob_whenValidRequest_thenJobIsCreated() {
-        SaveJobRequest request = new SaveJobRequest();
-        request.setName("Java Developer");
-        request.setDescription("Analyze requirements, design solutions and develop software artefact's");
-
-        Job job = jobService.createJob(request);
-
-        assertThat(job, notNullValue());
-        assertThat(job.getId(), greaterThan(0L));
-        assertThat(job.getName(), is(request.getName()));
-        assertThat(job.getDescription(), is(request.getDescription()));
+        createJob();
     }
+
+
 
     @Test
     void createJob_whenMissingName_thenExceptionIsThrown() {
@@ -45,6 +40,41 @@ public class JobServiceIntegrationTests {
             assertThat("Unexpected exception type. ", e instanceof TransactionSystemException);
         }
 
+    }
 
+    @Test
+    void getJob_whenExistingJob_thenReturnJob (){
+        Job job = createJob();
+
+        Job response = jobService.getJob(job.getId());
+
+        assertThat(response, notNullValue());
+        assertThat(response.getId(), is(job.getId()));
+        assertThat(response.getName(),is(job.getName()));
+        assertThat(response.getDescription(),is(job.getDescription()));
+
+    }
+
+    @Test
+    void getJob_whenNonExistingJob_thenThrowResourcesNotFoundException(){
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> jobService.getJob(99999));
+    }
+
+
+
+    private Job createJob() {
+        SaveJobRequest request = new SaveJobRequest();
+        request.setName("Java Developer");
+        request.setDescription("Analyze requirements, design solutions and develop software artefact's");
+
+        Job job = jobService.createJob(request);
+
+        assertThat(job, notNullValue());
+        assertThat(job.getId(), greaterThan(0L));
+        assertThat(job.getName(), is(request.getName()));
+        assertThat(job.getDescription(), is(request.getDescription()));
+
+        return job;
     }
 }
